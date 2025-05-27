05cb3db649473b9f0510a016bcf407621c9e7de0
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-05-27 16:56:00 -0700

    Implement user authentication features including login and registration forms, validation, and session management; update routes and styles accordingly.

---
+++ routes/accountRoute.js
@@ -0,0 +1,19 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const accountController = require("../controllers/accountController")
+const utilities = require('../utilities')
+const regValidate = require('../utilities/account-validation')
+
+
+// Route to build inventory by classification view
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+//Route to insert new user's data
+router.post("/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+     utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router;
